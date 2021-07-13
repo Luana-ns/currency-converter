1ba7fc734ef96f659a422f664390e8f02cea4111
Luana Nunes <lununes.s@outlook.com>
2021-07-12 22:21:27 -0300

    Inserindo funcao de conversao

---
+++ js/script.js
@@ -0,0 +1,10 @@
+function onClickConverter() {
+    
+    const valueToConvert = (document.getElementById("input-value")).value;
+    const convertedValue = document.getElementById("converted-value");
+    
+    convertedValue.value = valueToConvert * 5.10
+    
+
+};
+
