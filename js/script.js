873e621a3964eea2160cdeaf33a578c5f4ad879e
Luana Nunes <lununes.s@outlook.com>
2021-07-20 20:11:31 -0300

    Adicionando rascunho inicial para a função troca de moedas

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+// const loadCurrencyOptions = () => {
+//     document.getElementById("entrada").innerHTML = generateHtmlOptions(
+//       "Dolar"
+//     );
+//     document.getElementById("saida").innerHTML = generateHtmlOptions(
+//       "Real"
+//     );
+// };
+//   loadCurrencyOptions();
+
 function onClickConverter() {
     
     const valueToConvert = (document.getElementById("input-value")).value;
@@ -6,3 +16,11 @@ function onClickConverter() {
     
     convertedValue.value = (valueToConvert * 5.10).toFixed(2)
 };
+
+// function onClickSwap() {
+//     const entrada = document.getElementById('entrada');
+//     const saida = document.getElementById('saida');
+
+//     entrada = innerHtml(saida)
+//     saida = innerHtml(entrada)
+
